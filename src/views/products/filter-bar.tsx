f4c6793b52a4597fac27ad84a7031b47455ec963
goldendriver <goldendriver0731@gmail.com>
2022-10-30 23:35:08 +0900

    add components in filterbar component

---
+++ src/views/products/filter-bar.tsx
@@ -0,0 +1,33 @@
+import { Menu, Popover, Transition } from "@headlessui/react";
+import { Filters } from "../../components/Filters";
+import { AllCategories } from "../../components/AllCategories";
+import { Price } from "../../components/Price";
+import { ItemCondition } from "../../components/ItemCondition";
+import { Location } from "../../components/Location";
+
+export const FilterBar = () => {
+    return (
+        <div className="bg-header-pink flex justify-between items-center px-4 sm:px-6 md:justify-start md:space-x-10 mx-auto shadow-header h-60px" style={{ height: "60px" }}>
+            <Popover.Group as="nav" className="hidden lg:flex space-x-20 items-center pl-10">
+                <div className="font-light">
+                    <Filters />
+                </div>
+                <div className="font-light">
+                    <AllCategories />
+                </div>
+                <div className="font-light">
+                    <Price />
+                </div>
+                <div className="font-light text-white hover:text-gray-500 uppercase">
+                    <ItemCondition />
+                </div>
+                <div className="font-light text-white hover:text-gray-500 uppercase">
+                    <Location />
+                </div>
+            </Popover.Group>
+            <div className="" style={{ marginLeft: "50%" }}>
+                sd
+            </div>
+        </div>
+    )
+}
--- src/views/products/index.tsx
+++ src/views/products/index.tsx
@@ -1,9 +1,12 @@
 import { Page } from "../../components/Page";
+import { FilterBar } from "./filter-bar";
+import { ProductsList } from "./products-list";
 
 export const ProductsView = () => {
     return (
         <Page title="Products | DigitalEyes">
-
+            <FilterBar />
+            <ProductsList />
         </Page>
     );
 }
